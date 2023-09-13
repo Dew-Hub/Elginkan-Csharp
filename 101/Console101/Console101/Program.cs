7d8d7e52fd8096c4b5f13b480faa5843d7bd777c
Burak <burakonturk2000@gmail.com>
2023-09-13 21:30:25 +0300

    Hesap Makinesi

--- 101/Console101/Console101/Program.cs
+++ 101/Console101/Console101/Program.cs
@@ -1,4 +1,130 @@
-﻿int sayi = 4;
-Console.WriteLine("Hello, World!");
-Console.Write("Burak");
-Console.Write(" Öntürk \n" + sayi.ToString());
+﻿int girdisayi1;
+int girdisayi2;
+
+string tekrarla;
+string istenen;
+
+Baslat();
+
+
+void Baslat()
+{
+    
+
+    Console.Clear();
+
+    Console.WriteLine("\nHESAP MAKİNESİ");
+    Console.WriteLine("----------------");
+    Console.WriteLine("1) Toplama");
+    Console.WriteLine("2) Çıkarma");
+    Console.WriteLine("3) Bölme");
+    Console.WriteLine("4) Çarpma");
+    Console.WriteLine("----------------");
+
+    Console.Write("Yapılmak istenen işlem: ");
+    istenen = Console.ReadLine();
+
+    Console.Clear();
+
+
+
+    if (int.Parse(istenen) == 1)
+    {
+
+
+        Console.WriteLine("\nTOPLAMA");
+        Console.WriteLine("----------------");
+
+        Console.Write("İlk Sayıyı Giriniz: ");
+        string test1 = Console.ReadLine();
+
+        Console.Write("İkinci Sayıyı Giriniz: ");
+        string test2 = Console.ReadLine();
+
+        int toplam = Convert.ToInt32(test1) + Convert.ToInt32(test2);
+
+        Console.WriteLine("Sonuç: " + toplam);
+
+        Console.Write("Tekrar işlem yapmak istiyor musunuz? (Y/N): ");
+        tekrarla = Console.ReadLine();
+
+        if (tekrarla == "Y")
+            Baslat();
+        else;
+
+    }
+    else if (int.Parse(istenen) == 2)
+    {
+
+
+        Console.WriteLine("\nÇıkarma");
+        Console.WriteLine("----------------");
+
+        Console.Write("İlk Sayıyı Giriniz: ");
+        string test1 = Console.ReadLine();
+
+        Console.Write("İkinci Sayıyı Giriniz: ");
+        string test2 = Console.ReadLine();
+
+        int toplam = Convert.ToInt32(test1) - Convert.ToInt32(test2);
+
+        Console.WriteLine("Sonuç: " + toplam);
+
+        Console.Write("Tekrar işlem yapmak istiyor musunuz? (Y/N): ");
+        tekrarla = Console.ReadLine();
+
+        if (tekrarla == "Y")
+            Baslat();
+        else;
+    }
+    else if (int.Parse(istenen) == 3)
+    {
+
+
+        Console.WriteLine("\nBölme");
+        Console.WriteLine("----------------");
+
+        Console.Write("İlk Sayıyı Giriniz: ");
+        string test1 = Console.ReadLine();
+
+        Console.Write("İkinci Sayıyı Giriniz: ");
+        string test2 = Console.ReadLine();
+
+        float  toplam = Convert.ToSingle(test1) / Convert.ToSingle(test2);
+
+        Console.WriteLine("Sonuç: " + toplam);
+
+        Console.Write("Tekrar işlem yapmak istiyor musunuz? (Y/N): ");
+        tekrarla = Console.ReadLine();
+
+        if (tekrarla == "Y")
+            Baslat();
+        else;
+    }
+    else if (int.Parse(istenen) == 4)
+    {
+
+
+        Console.WriteLine("\nÇarpma");
+        Console.WriteLine("----------------");
+
+        Console.Write("İlk Sayıyı Giriniz: ");
+        string test1 = Console.ReadLine();
+
+        Console.Write("İkinci Sayıyı Giriniz: ");
+        string test2 = Console.ReadLine();
+
+        int toplam = Convert.ToInt32(test1) * Convert.ToInt32(test2);
+
+        Console.WriteLine("Sonuç: " + toplam);
+
+        Console.Write("Tekrar işlem yapmak istiyor musunuz? (Y/N): ");
+        tekrarla = Console.ReadLine();
+
+        if (tekrarla == "Y")
+            Baslat();
+        else;
+    }
+
+}
+
