613f67fb2b10a837af82bbec8014545325929527
Burak <burakonturk2000@gmail.com>
2023-09-27 19:02:16 +0300

    3. hafta 1. gün

---
+++ 105/Console103/Console103/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        DenizAraci da = new DenizAraci();
+
+        da.araccinsi = "Motoryat";
+        da.agirligi = 5000;
+        da.hizi = 20;
+
+        Console.Read();
+
+    }
+}
+
+
+class DenizAraci
+{
+    public string araccinsi;
+    public int agirligi;
+    public byte hizi;
+
+    public void HizGoster()
+    {
+        Console.WriteLine(hizi);
+    }
+
+}
